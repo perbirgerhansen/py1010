6605640205d764e221048eabcea876bce7c8fffe
perbirgerhansen <perbirgerhansen@outlook.com>
2024-10-29 20:22:14 +0100

    Add files via upload

---
+++ fra_C_til_F_v01.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Omregningen fra C til F
+Spyder Editor
+
+@autor: Per 
+"""
+print (" 2.7 Oppgaver til kapittel 2")
+print (" Oppgave 2.1 Python som kalkulator")
+
+L = 100000
+R = 23.4
+MND = 12
+SUMAAR = L/R*100
+print ("Sum pr. år", SUMAAR)
+print("sum pr. måned", SUMAAR/MND)
+
+print ((100000/23.4*100)/12)
+
+
+
+
+
